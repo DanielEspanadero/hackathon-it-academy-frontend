c85fe03512eb23b9725163e43bfc2de17858932d
Daniel Españadero <developer@daniel-espanadero.com>
2022-07-22 01:48:59 +0200

    Header created

---
+++ src/components/header/Header.tsx
@@ -0,0 +1,9 @@
+import { HeaderStyled } from "../../styles/header/HeaderStyled"
+
+export const Header = () => {
+  return (
+    <HeaderStyled>
+        <div className="charizard"></div>
+    </HeaderStyled>
+  )
+}
---
+++ src/styles/header/HeaderStyled.tsx
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const HeaderStyled = styled.div`
+.charizard{
+    width: 100%;
+    height: 30vh;
+    background-image: url('../../../assets/images/header/wallpaper.webp');
+    background-size: cover;
+    background-repeat: no-repeat;
+    background-position: center;
+}
+`;
